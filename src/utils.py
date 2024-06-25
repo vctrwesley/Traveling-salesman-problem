faaefbcf94d05d94fe96a77fd0dc4c46fa3d44ea
Felipe Nogueira <felipe.leite23@gmail.com>
2024-06-25 17:43:31 -0300

    Add TSP algorithm selection and dataset loading functionality

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,9 @@
-from algoritmos import hillClimbing, geneticAlgorithm, simulatedAnnealing, tabuSearch
 from time import time
+import os
 
-def tspAlgorithms(algorithm, dataPath, process):
+def tspAlgorithms(algorithm, dataPath, process, initialTour=None):
     numCities, distanceMatrix = loadData(dataPath) 
-    args = (numCities, distanceMatrix)
+    args = (numCities, distanceMatrix, initialTour)
     memoryUsed = process.memory_info().rss / 1024.0
     start = time()
     result = algorithm(*args)
@@ -11,8 +11,24 @@ def tspAlgorithms(algorithm, dataPath, process):
     timeSpent = end - start
     memoryUsed = process.memory_info().rss / 1024.0 - memoryUsed
     return result, memoryUsed, timeSpent
-    
-    
+
+def selectDataset(dataPath):
+    files = os.listdir(dataPath)
+    print("Available datasets:")
+    for i, file in enumerate(files):
+        file = file.split(".")[0]
+        print(f"{i+1}. {file}")
+    while True:
+        try:
+            choice = int(input("Select a dataset: "))
+            if choice < 1 or choice > len(files):
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid choice. Please try again.")
+    selectedFile = files[choice-1]
+    return os.path.join(dataPath, selectedFile)
+
 def loadData(filename):
     with open(filename, "r") as file:
         distanceMatrix = []
